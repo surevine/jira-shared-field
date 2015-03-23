ed11db6ea75d5db1f74dd74710274c2703a21f66
Dave Cridland <dave.cridland@surevine.com>
2015-03-23 14:34:21 +0000

    Added custom field

---
+++ src/test/java/ut/com/surevine/jira/customfields/SharedFieldTest.java
@@ -0,0 +1,34 @@
+package ut.com.surevine.jira.customfields;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import com.surevine.jira.customfields.SharedField;
+
+import static org.mockito.Mockito.*;
+
+/**
+ * @since 3.5
+ */
+public class SharedFieldTest {
+
+    @Before
+    public void setup() {
+
+    }
+
+    @After
+    public void tearDown() {
+
+    }
+
+    @Test(expected=Exception.class)
+    public void testSomething() throws Exception {
+
+        //SharedField testClass = new SharedField();
+
+        throw new Exception("SharedField has no tests!");
+
+    }
+
+}
