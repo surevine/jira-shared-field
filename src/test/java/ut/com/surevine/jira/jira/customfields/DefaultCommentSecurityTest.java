f743fb314e1483d7bcabf0b5f2981c1ccf159fe1
Dave Cridland <dave.cridland@surevine.com>
2015-03-24 12:20:38 +0000

    Add comment level field

---
+++ src/test/java/ut/com/surevine/jira/jira/customfields/DefaultCommentSecurityTest.java
@@ -0,0 +1,34 @@
+package ut.com.surevine.jira.jira.customfields;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import com.surevine.jira.jira.customfields.DefaultCommentSecurity;
+
+import static org.mockito.Mockito.*;
+
+/**
+ * @since 3.5
+ */
+public class DefaultCommentSecurityTest {
+
+    @Before
+    public void setup() {
+
+    }
+
+    @After
+    public void tearDown() {
+
+    }
+
+    @Test(expected=Exception.class)
+    public void testSomething() throws Exception {
+
+        //DefaultCommentSecurity testClass = new DefaultCommentSecurity();
+
+        throw new Exception("DefaultCommentSecurity has no tests!");
+
+    }
+
+}
